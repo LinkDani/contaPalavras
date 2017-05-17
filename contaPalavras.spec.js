0d1d24d237dbc235c6377a0d4c666b04ea94abf9
Daniel Walter <danielwalterpand@gmail.com>
2017-05-16 21:06:19 -0300

    Criação do primeiro teste

---
+++ contaPalavras.spec.js
@@ -0,0 +1,13 @@
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const { expect } = require('code');
+
+const { contaPalavras } = require('../contaPalavras');
+
+lab.experiment('Quando informa a frase', () => {
+  lab.test('Deve retornar com o número de palavras:' (done) => {
+    expect(words('Meu nome é Bond, James Bond'));
+    done();
+  });
+});
